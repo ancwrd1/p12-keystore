cc4cb91584819817da153261d46008ebd4d1c553
Pankratov Dmitry <dmitry@pankratov.net>
2024-03-19 23:32:12 +0100

    Make MacAlgorithm also non-exhaustive

--- src/keystore.rs
+++ src/keystore.rs
@@ -289,6 +289,7 @@ impl EncryptionAlgorithm {
 
 /// MAC algorithm to use when creating the PKCS#12 file
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[non_exhaustive]
 pub enum MacAlgorithm {
     HmacSha1,
     HmacSha256,
